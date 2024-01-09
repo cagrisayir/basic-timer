b505b4ed4bd3b57990432f9950fae16d92d26a5e
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-09 09:46:41 +0300

    stop the timer and reset it

--- basic-timer/ContentView.swift
+++ basic-timer/ContentView.swift
@@ -9,16 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var secondsElapsed = 0
+    // to stop the timer
+    @State private var timerTask: Task<Void, Error>?
 
     var body: some View {
         Form {
             Section {
-                Button("Start timer") {
-                    secondsElapsed = 0
-                    Task {
-                        while true {
-                            try await Task.sleep(for: .seconds(1))
-                            secondsElapsed += 1
+                if timerTask == nil {
+                    Button("Start timer") {
+                        secondsElapsed = 0
+                        timerTask?.cancel()
+                        timerTask = Task {
+                            while true {
+                                try await Task.sleep(for: .seconds(1))
+                                secondsElapsed += 1
+                            }
+                        }
+                    }
+                } else {
+                    Button {
+                        timerTask?.cancel()
+                        timerTask = nil
+                    } label: {
+                        HStack {
+                            Text("Stop timer")
+                            Spacer()
+                            ProgressView().id(UUID())
                         }
                     }
                 }
