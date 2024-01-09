dccf26e0dc8237f44dc987fe8c8c2c93276a5218
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-09 09:34:37 +0300

    Initial Commit

---
+++ basic-timer/basic_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  basic_timerApp.swift
+//  basic-timer
+//
+//  Created by Omer Cagri Sayir on 9.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct basic_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
