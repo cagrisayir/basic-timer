f017c559200c628d780e9f0afada6056e9b91a87
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-09 09:42:56 +0300

    timer on

--- basic-timer/ContentView.swift
+++ basic-timer/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var secondsElapsed = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Timer!")
+        Form {
+            Section {
+                Button("Start timer") {
+                    secondsElapsed = 0
+                    Task {
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            secondsElapsed += 1
+                        }
+                    }
+                }
+                Text("Time: \(secondsElapsed)")
+            } header: {
+                Text("Timer")
+            }
         }
-        .padding()
     }
 }
 
